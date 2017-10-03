261fdc7d8c1fc0315ae29b86c028dc396a923766
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2017-10-03 17:02:49 +0900

    Add menu

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   resources :indivisuals
   resources :organisms
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "organisms#index"
 end
